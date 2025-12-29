aaf8cc63093ec18d99c6fddbd850de11c6c24394
gauravdiwakar961-code <gauravdiwakar961@gmail.com>
2025-12-29 10:11:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,11 @@ scaler = StandardScaler()
 scaled_X = scaler.fit_transform(final_X)
 
 st.sidebar.title('Select House features: ')
-st.sidebar.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfUjVgK1ruF1UhBk-PlcaBTMmrw_7WlNBJhg&s')
+st.sidebar.image('https://media.tenor.com/ClsDaM8C7cYAAAAM/dancing-house.gif')
 all_value = []
 for i in final_X:
   result = st.sidebar.slider(f'Select {i} value')
   all_value.append(result)
 st.write(all_value)
+
 
